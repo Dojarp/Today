a1bb0b1fffbd30f195491c991d3a0de5436293d5
Dojarp <142612622+Dojarp@users.noreply.github.com>
2026-09-10 00:52:58 +0530

    Fix static frontend serving

--- server.js
+++ server.js
@@ -20,10 +20,10 @@ const ai = new GoogleGenAI({
 app.use(express.json());
 
 // Serve frontend
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 
